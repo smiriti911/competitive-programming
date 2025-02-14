533b174e86163af5c72a06183dd28849986963af
smiriti <148301685+smiriti911@users.noreply.github.com>
2025-02-14 14:51:28 +0530

    Time: 0 ms (100%), Space: 41.2 MB (81.42%) - LeetHub

---
+++ 0092-reverse-linked-list-ii/0092-reverse-linked-list-ii.java
@@ -0,0 +1,41 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode reverseBetween(ListNode head, int left, int right) {
+        if (head == null || head.next == null || left == right) return head;
+
+        ListNode mainHead = new ListNode(0, head);  
+        ListNode prev = mainHead;
+        ListNode next = null;
+
+        // Move `prev` to the node just before `left`
+        for (int i = 0; i < left - 1; i++) {
+            prev = prev.next;
+        }
+
+        ListNode curr = prev.next;
+        ListNode newEnd = curr; // This node will be the end of the reversed part
+        ListNode last = prev;   // Corrected `last` position
+
+        // Reverse the sublist
+        for (int i = left; i <= right; i++) {  // Fixed the loop range
+            next = curr.next;
+            curr.next = prev.next;  // Attach `curr` to the reversed part
+            prev.next = curr;       // Move `prev` forward
+            curr = next;            // Move `curr` forward
+        }
+
+        // Connect the reversed part with the remaining list
+        newEnd.next = curr;  
+
+        return mainHead.next; // Return the modified list
+    }
+}
