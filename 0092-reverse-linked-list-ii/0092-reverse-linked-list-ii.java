1456df94e63ce4152205ab2691259a065a498da4
smiriti <148301685+smiriti911@users.noreply.github.com>
2025-02-14 14:59:48 +0530

    Time: 0 ms (100%), Space: 41.5 MB (39.69%) - LeetHub

--- 0092-reverse-linked-list-ii/0092-reverse-linked-list-ii.java
+++ 0092-reverse-linked-list-ii/0092-reverse-linked-list-ii.java
@@ -23,17 +23,17 @@ class Solution {
 
         ListNode curr = prev.next;
         ListNode newEnd = curr; // This node will be the end of the reversed part
-        ListNode last = prev;   // Corrected `last` position
-
+        ListNode last=prev;
         // Reverse the sublist
         for (int i = left; i <= right; i++) {  // Fixed the loop range
             next = curr.next;
-            curr.next = prev.next;  // Attach `curr` to the reversed part
-            prev.next = curr;       // Move `prev` forward
+            curr.next = prev;  // Attach `curr` to the reversed part
+            prev = curr;       // Move `prev` forward
             curr = next;            // Move `curr` forward
         }
 
         // Connect the reversed part with the remaining list
+        last.next=prev;
         newEnd.next = curr;  
 
         return mainHead.next; // Return the modified list
