b8b52f2f1f9ff52b7f88b0ae7947db7cdae2704b
smiriti <148301685+smiriti911@users.noreply.github.com>
2025-02-20 18:07:47 +0530

    Time: 27 ms (32.3%), Space: 45.7 MB (5.1%) - LeetHub

--- 2227-sum-of-subarray-ranges/2227-sum-of-subarray-ranges.java
+++ 2227-sum-of-subarray-ranges/2227-sum-of-subarray-ranges.java
@@ -1,68 +1,87 @@
 class Solution {
-    class Node{
-        long val, displace;
-        Node(long val, long displace){
-            this.val = val;
-            this.displace = displace;
-        }
-    }
-    public long subArrayRanges(int[] nums) {
-        
-        //lesser than current element
-        Stack<Node> stack = new Stack<>();
-        //from left
-        long [] lesserLeft = new long[nums.length];
-        for (int i = 0; i< nums.length; i++){
-            long count = 1;
-            while(stack.size()>0 && stack.peek().val<=nums[i]){
-                count+=stack.pop().displace;
+    public long subArrayRanges(int[] arr) {
+        int n=arr.length;
+        int [] nge=new int [n];
+        int [] nse=new int [n];
+        int [] pse=new int [n];
+        int [] pge=new int [n];
+        long totalMax=0;
+        long totalMin=0;
+        Stack <Integer> stack=new Stack<>();
+
+        for(int i=n-1; i>=0; i--){
+            while(!stack.isEmpty() && arr[stack.peek()]<= arr[i]){
+                stack.pop();
             }
-            stack.add(new Node(nums[i], count));
-            lesserLeft[i] = count;
-        }
-        stack.clear();
-        //from right
-        long[] lesserRight = new long[nums.length];
-        for (int i = nums.length-1; i>=0; i--){
-            long count = 1;
-            while(stack.size()>0 && stack.peek().val<nums[i]){
-                count+=stack.pop().displace;
+            if(!stack.isEmpty()){
+                nge[i]=stack.peek();
             }
-            stack.add(new Node(nums[i], count));
-            lesserRight[i] = count;
-        }
-        
-        
-        //greater than current element
-        stack.clear();
-        //from left
-        long [] greaterLeft = new long[nums.length];
-        for (int i = 0; i< nums.length; i++){
-            long count = 1;
-            while(stack.size()>0 && stack.peek().val>=nums[i]){
-                count+=stack.pop().displace;
+            else{
+                nge[i]=n;
             }
-            stack.add(new Node(nums[i], count));
-            greaterLeft[i] = count;
+            stack.push(i);
         }
-        stack.clear();
-        //from right
-        long[] greaterRight = new long[nums.length];
-        for (int i = nums.length-1; i>=0; i--){
-            long count = 1;
-            while(stack.size()>0 && stack.peek().val>nums[i]){
-                count+=stack.pop().displace;
+        while(!stack.isEmpty()){
+            stack.pop();
+        }
+        
+        for(int i=0; i<n; i++){
+            while(!stack.isEmpty() && arr[stack.peek()]< arr[i]){
+                stack.pop();
             }
-            stack.add(new Node(nums[i], count));
-            greaterRight[i] = count;
-        }        
-        
-        long ans = 0;
-        //Now we subtract the count of minimum occurrences from the count of maximum occurrences
-        
-        for (int i = 0; i< nums.length; i++){
-            ans+=((lesserLeft[i]*lesserRight[i]) - (greaterLeft[i]*greaterRight[i]))*nums[i];
+            if(!stack.isEmpty()){
+                pge[i]=stack.peek();
+            }
+            else{
+                pge[i]=-1;
+            }
+            stack.push(i);
+
+            long left=i-pge[i];
+            long right=nge[i]-i;
+             
+            totalMax+= left*right*arr[i];
+            
         }
-        return ans;
+          while(!stack.isEmpty()){
+            stack.pop();
+        }
+
+         for(int i=n-1; i>=0; i--){
+            while(!stack.isEmpty() && arr[stack.peek()]>= arr[i]){
+                stack.pop();
+            }
+            if(!stack.isEmpty()){
+                nse[i]=stack.peek();
+            }
+            else{
+                nse[i]=n;
+            }
+            stack.push(i);
+        }
+        while(!stack.isEmpty()){
+            stack.pop();
+        }
+        
+        for(int i=0; i<n; i++){
+            while(!stack.isEmpty() && arr[stack.peek()]>arr[i]){
+                stack.pop();
+            }
+            if(!stack.isEmpty()){
+                pse[i]=stack.peek();
+            }
+            else{
+                pse[i]=-1;
+            }
+            stack.push(i);
+
+            long left=i-pse[i];
+            long right=nse[i]-i;
+             
+            totalMin+= left*right*arr[i];
+            
+        }
+
+        return totalMax-totalMin;
     }
 }
