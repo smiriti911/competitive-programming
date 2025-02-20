19204c568267432af3c33373d20bdee274f1bc06
smiriti <148301685+smiriti911@users.noreply.github.com>
2025-02-20 15:35:37 +0530

    Time: 19 ms (99.63%), Space: 50.7 MB (93.45%) - LeetHub

---
+++ 0937-online-stock-span/0937-online-stock-span.java
@@ -0,0 +1,50 @@
+class StockSpanner {
+    private static class Node {
+        Node next;
+        int height;
+        int count;
+
+        static Node LAST = new Node();
+
+        static {
+            LAST.height = 0x7FFF_FFFF;
+            LAST.count = 0;
+        }
+    }
+
+    private Node head = Node.LAST;
+
+    private void insert(int height) {
+        if (height == head.height)
+            head.count += 1;
+        else {
+            Node n = new Node();
+
+            n.height = height;
+            n.count = 1;
+
+            while (n.height >= head.height) {
+                n.count += head.count;
+
+                head = head.next;
+            }
+
+            n.next = head;
+            head = n;
+        }
+    }
+
+    public StockSpanner() {
+    }
+
+    public int next(int price) {
+        insert(price);
+
+        return head.count;
+    }
+}
+/**
+ * Your StockSpanner object will be instantiated and called as such:
+ * StockSpanner obj = new StockSpanner();
+ * int param_1 = obj.next(price);
+ */
