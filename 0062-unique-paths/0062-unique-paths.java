a39683521d55cea7a8c4e7e51df8dd01b8d5c53f
smiriti <148301685+smiriti911@users.noreply.github.com>
2025-02-26 14:15:30 +0530

    Time: 93 ms (2.34%), Space: 40.4 MB (75.55%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -1,30 +1,21 @@
 class Solution {
-    private int[][] dp;
+    public int uniquePaths(int m, int n) {
+        int[][] dp = new int[m][n];
 
-    public int countPaths(int m, int n){
-        if(m==1 && n==1){
-            return 1;
-        }
-        if(m<0|| n<0){
-            return 0;
-        }
-         if(dp[m][n]!=0){
-            return dp[m][n];
-        }else{
-           int left=countPaths(m, n-1);
-           int up=countPaths(m-1, n);
+        // Initialize first row and first column to 1
+        for (int i = 0; i < m; i++) dp[i][0] = 1;
+        for (int j = 0; j < n; j++) dp[0][j] = 1;
 
-           dp[m][n]=left+up;
-        }
-        
+        // Fill the DP table
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                int up = dp[i - 1][j];   // Paths from top
+                int left = dp[i][j - 1]; // Paths from left
 
-        return dp[m][n];
-        
-    }
-    public int uniquePaths(int m, int n)
-    {
-        dp= new int[m+1][n+1];
-        return countPaths(m,n);
-       
+                dp[i][j] = up + left; // Sum of both directions
+            }
+        }
+
+        return dp[m - 1][n - 1]; // Bottom-right cell has the answer
     }
 }
