8427c1d937fba4737b9e685e8a7324309d6704df
smiriti <148301685+smiriti911@users.noreply.github.com>
2025-03-07 20:01:17 +0530

    Time: 411 ms (16%), Space: 45.3 MB (55.6%) - LeetHub

--- 2610-closest-prime-numbers-in-range/2610-closest-prime-numbers-in-range.java
+++ 2610-closest-prime-numbers-in-range/2610-closest-prime-numbers-in-range.java
@@ -1,36 +1,35 @@
 class Solution {
    public static boolean isPrime(int n) {
-        if (n <= 1) return false; // Numbers <=1 are not prime
-        if (n <= 3) return true; // 2 and 3 are prime
+        if (n <= 1) return false; 
+        if (n <= 3) return true; 
 
-        if (n % 2 == 0 || n % 3 == 0) return false; // Eliminate multiples of 2 and 3
+        if (n % 2 == 0 || n % 3 == 0) return false; 
 
-        // Check divisibility from 5 to √n, skipping even numbers
         for (int i = 5; i * i <= n; i += 6) {
             if (n % i == 0 || n % (i + 2) == 0) return false;
         }
         return true;
     }
     public int[] closestPrimes(int left, int right) {
-        List<Integer> arr=new ArrayList<>();
+        List<Integer> list=new ArrayList<>();
         for(int i=left; i<=right; i++){
             if(isPrime(i)){
-               arr.add(i);
+               list.add(i);
             }
         }
-        Integer[] list = arr.toArray(new Integer[0]);
-        int n=list.length;
+    
+        int n=list.size();
         int min=Integer.MAX_VALUE;
         int [] ans={-1, -1};
         if(n<=1){
             return ans;
         }
         for(int i=0; i+1<n; i++){
-          int m=list[i+1]-list[i];
+          int m=list.get(i+1)-list.get(i);
           if(min>m){
             min=m;
-            ans[0]= list[i];
-            ans[1]=list[i+1];
+            ans[0]= list.get(i);
+            ans[1]=list.get(i+1);
           }
         }
         return ans;
