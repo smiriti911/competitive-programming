f6148b005478f995cc6c809be9dbe6b9ef6b8d5e
smiriti <148301685+smiriti911@users.noreply.github.com>
2025-07-10 16:21:40 +0530

    Time: 1 ms (64.06%), Space: 42.1 MB (77.91%) - LeetHub

--- 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.java
+++ 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.java
@@ -1,55 +1,58 @@
 /**
  * Definition for a binary tree node.
  * public class TreeNode {
- * int val;
- * TreeNode left;
- * TreeNode right;
- * TreeNode() {}
- * TreeNode(int val) { this.val = val; }
- * TreeNode(int val, TreeNode left, TreeNode right) {
- * this.val = val;
- * this.left = left;
- * this.right = right;
- * }
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
  * }
  */
 class Solution {
     public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
-        Queue<TreeNode> q = new LinkedList<>();
-        List<List<Integer>> result = new ArrayList<>();
-        if (root == null) {
-            return result;
-        }
+        List<List<Integer>> list= new ArrayList<>();
+        Queue<TreeNode> q= new LinkedList<>();
 
+        int lvl=1;
         q.add(root);
-         int lvl=1;
-        while (!q.isEmpty()) {
-            List<Integer> sublist = new ArrayList<>();
-            int level = q.size();
-       
-            for (int i = 0; i < level; i++) {
-                TreeNode node = q.remove();
-                if(lvl%2==0){
-                   sublist.addFirst(node.val);
-                }
-                else{
-                    sublist.addLast(node.val);
-                }
-                
 
-                if (node.left != null) {
-                    q.add(node.left);
-                }
-                
-                if (node.right != null) {
-                    q.add(node.right);
-                }
-
-            }
-            result.add(sublist);
-            lvl++;
-
+        if(root==null){
+            return list;
         }
-        return result;
+
+        while(!q.isEmpty()){
+          List<Integer> sublist= new ArrayList<>();
+          
+          int n=q.size();
+
+          for(int i =0; i<n; i++){
+
+            TreeNode node= q.remove();
+            if(lvl%2==0){
+               
+             sublist.addFirst(node.val);
+
+            }else{
+                sublist.addLast(node.val);
+            }
+
+            if(node.left!=null){
+                q.add(node.left);
+            }
+
+            if(node.right!=null){
+                q.add(node.right);
+            }
+          }
+          lvl++;
+          list.add(sublist);
+        }
+
+        return list;
     }
 }
