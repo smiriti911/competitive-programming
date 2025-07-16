ee46ce20cd053f71536d0a87a7ead0795c974dc9
smiriti <148301685+smiriti911@users.noreply.github.com>
2025-07-16 15:49:15 +0530

    Time: 2 ms (99.77%), Space: 50.5 MB (41.97%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.java
+++ 0229-majority-element-ii/0229-majority-element-ii.java
@@ -1,24 +1,55 @@
 class Solution {
     public List<Integer> majorityElement(int[] nums) {
 
-        List<Integer> result = new ArrayList<>();
-        Map<Integer, Integer> map = new HashMap<>();
+        int count1 = 0;
+        int count2 = 0;
 
-        int n = nums.length;
+        int candidate1 = 0;
+        int candidate2 = 0;
 
-        for (int i = 0; i < n; i++) {
-            map.put(nums[i], map.getOrDefault(nums[i], 0) + 1);
-        }
-        for (int i = 0; i < n; i++) {
-            int count = map.get(nums[i]);
+        for (int i=0; i < nums.length; i++) {
 
-            if (count > (n / 3)) {
-                if (!result.contains(nums[i])) {
-                    result.add(nums[i]);
-                }
+            if (count1 == 0 && nums[i] != candidate2) {
+                count1 = 1;
+                candidate1 = nums[i];
+            } else if (count2 == 0 && nums[i] != candidate1) {
+                count2 = 1;
+                candidate2 = nums[i];
+            } else if (candidate1 == nums[i]) {
+                count1++;
+            } else if (candidate2 == nums[i]) {
+                count2++;
+            }
+
+            else {
+                count1--;
+                count2--;
             }
         }
 
+        List<Integer> result = new ArrayList<>();
+        int threshold = nums.length / 3; 
+
+
+        count1 = 0;
+        count2 = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (candidate1 == nums[i]) {
+                count1++;
+            } else if (candidate2 == nums[i]) {
+                count2++;
+            }
+        }
+
+     
+        if (count1 > threshold) {
+            result.add(candidate1);
+        }
+        if (count2 > threshold) {
+            result.add(candidate2);
+        }
+
         return result;
+
     }
 }
