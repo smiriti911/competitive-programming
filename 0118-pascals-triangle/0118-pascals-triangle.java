6eae27681f6804ae9a688abbe9bf9bf9e31baf53
smiriti <148301685+smiriti911@users.noreply.github.com>
2025-07-16 13:27:52 +0530

    Time: 1 ms (83.15%), Space: 42.5 MB (7.63%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.java
+++ 0118-pascals-triangle/0118-pascals-triangle.java
@@ -1,23 +1,28 @@
 class Solution {
-    private long combination (int n, int r){
+
+    public long combination (int n, int r){
         long result=1;
-        for(long i=0; i<r; i++){
-            result= result*(n-i);
+
+        for(int i=0; i<r; i++){
+            result=result*(n-i);
             result=result/(i+1);
         }
+
         return result;
     }
     public List<List<Integer>> generate(int numRows) {
-        List<List<Integer>> ans =new ArrayList<>();
-        
+        List<List<Integer>> ans= new ArrayList<>();
+
         for(int i=0; i<numRows; i++){
             List<Integer> sublist=new ArrayList<>();
-         for(int j=0; j<=i; j++){
-            sublist.add((int)combination(i, j));
+
+            for(int j=0; j<=i; j++){
+                sublist.add((int)combination(i, j));
+            }
+
+            ans.add(sublist);
         }
 
-          ans.add(sublist);
-        }
-     return ans;
+        return ans;
     }
 }
