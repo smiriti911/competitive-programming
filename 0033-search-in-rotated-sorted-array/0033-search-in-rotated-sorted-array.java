e06635968dce2d37b0a00552b0ee1ee632d9cea5
smiriti <148301685+smiriti911@users.noreply.github.com>
2025-07-18 09:36:36 +0530

    Time: 0 ms (100%), Space: 42 MB (83.04%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -1,29 +1,59 @@
 class Solution {
     public int search(int[] nums, int target) {
-        int start = 0;
-        int end = nums.length - 1;
+        int left = 0;
+        int right = nums.length - 1;
+        int pivot = 0;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
 
-        while (start <= end) {
-            int mid = start + (end - start) / 2;
-            if (nums[mid] == target) {
-                return mid;
+            if (mid < right && nums[mid] > nums[mid + 1]) {
+                pivot = mid + 1;
+                break;
             }
-            else if (nums[start] <= nums[mid]) {
 
-                if (nums[start] <=target && target <= nums[mid]) {
-                    end = mid - 1;
-                } else {
-                    start = mid + 1;
-                }
+            if (mid > left && nums[mid - 1] > nums[mid]) {
+                pivot = mid;
+                break;
+            }
+
+            if (nums[mid] >= nums[left]) {
+                left = mid + 1;
             } else {
-                if (nums[mid] <= target && target <=nums[end]) {
-                    start = mid + 1;
-                } else {
-                    end = mid - 1;
-                }
+                right = mid - 1;
             }
         }
-        return -1;
 
+         left=pivot;
+         right=nums.length-1;
+
+        while(left<=right){
+            int mid= left+(right-left)/2;
+
+            if(nums[mid]==target){
+                return mid;
+            }
+            else if(nums[mid]>target){
+                right=mid-1;
+            }else{
+                left=mid+1;
+            }
+        }
+
+        right=pivot-1;
+        left=0;
+
+        while(left<=right){
+            int mid=left+(right-left)/2;
+
+            if(nums[mid]==target){
+                return mid;
+            }else if(nums[mid]>target){
+                right=mid-1;
+            }else{
+                left=mid+1;
+            }
+
+        }
+      return -1;
     }
 }
