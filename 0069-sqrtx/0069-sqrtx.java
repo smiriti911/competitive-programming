b11713295d0a7a0b52e3d95e4df2fceaadfc8b45
smiriti <148301685+smiriti911@users.noreply.github.com>
2025-07-18 20:53:40 +0530

    Time: 1 ms (85.54%), Space: 41.2 MB (20.43%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.java
+++ 0069-sqrtx/0069-sqrtx.java
@@ -1,25 +1,25 @@
 class Solution {
     public int mySqrt(int x) {
-        if(x==0 || x==1){
-            return x;
+        // Edge case: square root of 0 is 0
+        if (x == 0) return 0;
+
+        int left = 1, right = x;
+        int ans = 0;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            long square = (long) mid * mid;
+
+            if (square == x) {
+                return mid; // perfect square
+            } else if (square < x) {
+                ans = mid; // store as possible answer
+                left = mid + 1; // look for bigger
+            } else {
+                right = mid - 1; // look for smaller
+            }
         }
 
-        int start=2;
-        int end=x;
-        
-        while(start<=end){
-            int mid= start+(end-start)/2;
-
-            if(mid*mid==x){
-                return mid;
-            }
-            else if(mid*mid<x){
-                start=mid+1;
-            }else if((long)mid*mid>(long)x){
-                end=mid-1;
-            }
-        }
-
-        return end;
+        return ans; // closest integer sqrt
     }
 }
