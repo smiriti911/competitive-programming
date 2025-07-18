545d590c048ce7e654fac1abf6f5a5511c51c7ff
smiriti <148301685+smiriti911@users.noreply.github.com>
2025-07-18 20:52:26 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.java
+++ 0069-sqrtx/0069-sqrtx.java
@@ -1,24 +1,25 @@
 class Solution {
     public int mySqrt(int x) {
-        if(x==0||x==1){
+        if(x==0 || x==1){
             return x;
         }
 
         int start=2;
         int end=x;
-
+        
         while(start<=end){
-            int mid=start+(end-start)/2;
+            int mid= start+(end-start)/2;
+
             if(mid*mid==x){
                 return mid;
             }
-            else if((long)mid*mid>(long)x){
-                end=mid-1;
-            }
             else if(mid*mid<x){
                 start=mid+1;
+            }else if((long)mid*mid>(long)x){
+                end=mid-1;
             }
         }
+
         return end;
     }
 }
