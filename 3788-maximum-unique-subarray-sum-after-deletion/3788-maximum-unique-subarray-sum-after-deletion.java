9fcd3840bb095c014cf6016ebb18710488b1ba2b
Smiriti Kapoor <148301685+smiriti911@users.noreply.github.com>
2025-07-25 18:17:27 +0530

    Time: 3 ms (20.94%), Space: 42.5 MB (64.69%) - LeetHub

--- 3788-maximum-unique-subarray-sum-after-deletion/3788-maximum-unique-subarray-sum-after-deletion.java
+++ 3788-maximum-unique-subarray-sum-after-deletion/3788-maximum-unique-subarray-sum-after-deletion.java
@@ -2,20 +2,16 @@ class Solution {
     public int maxSum(int[] nums) {
 
         Set<Integer> set = new HashSet<>();
-
+        int sum = 0;
         for (int i = 0; i < nums.length; i++) {
             if (!set.contains(nums[i])) {
                set.add(nums[i]);
+               if(nums[i]>0){
+                sum+=nums[i];
+               }
             }
         }
         Integer[] arr = set.toArray(new Integer[0]);
-        int sum = 0;
-        for (int i = 0; i < arr.length; i++) {
-            if(arr[i]>0){
-                sum+=arr[i];
-            }
-        }
-       
         if (sum <= 0) {
             sum=Integer.MIN_VALUE;
             for (int i = 0; i < arr.length; i++) {
