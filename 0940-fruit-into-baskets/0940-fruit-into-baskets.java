3504f94c593bf10fd13f9751ffd4347b5d2c71c3
Smiriti Kapoor <148301685+smiriti911@users.noreply.github.com>
2025-07-25 12:27:13 +0530

    Time: 53 ms (24.13%), Space: 55.3 MB (40.71%) - LeetHub

--- 0940-fruit-into-baskets/0940-fruit-into-baskets.java
+++ 0940-fruit-into-baskets/0940-fruit-into-baskets.java
@@ -5,29 +5,25 @@ class Solution {
 
         int maxLen=0;
 
-        Map<Integer, Integer> map =new HashMap<>();
+        Map <Integer, Integer> map= new HashMap<>();
 
         while(r<nums.length){
             map.put(nums[r], map.getOrDefault(nums[r], 0)+1);
 
             if(map.size()>2){
-                while(map.size()>2){
-                    if(map.containsKey(nums[l])){
-                        int newVal=map.get(nums[l])-1;
-                        if(newVal>0){
-                            map.put(nums[l], newVal);
-                        }else{
-                            map.remove(nums[l]);
-                        }
-                    }
-                  l++;
+                map.put(nums[l], map.getOrDefault(nums[l], 0)-1);
+                if(map.get(nums[l])==0){
+                    map.remove(nums[l]);
+                
                 }
+                  l++;
+                  
             }
-
             if(map.size()<=2){
-                maxLen=Math.max(maxLen, r-l+1);
+                maxLen= Math.max(maxLen, r-l+1);
+              
             }
-            r++;
+              r++;
         }
 
         return maxLen;
