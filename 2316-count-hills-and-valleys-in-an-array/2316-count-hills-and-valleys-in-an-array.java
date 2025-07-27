e78aef6ccdce8817a701f3d264195f7f9721281b
Smiriti Kapoor <148301685+smiriti911@users.noreply.github.com>
2025-07-27 09:44:42 +0530

    Time: 1 ms (33.4%), Space: 41.9 MB (30.53%) - LeetHub

--- 2316-count-hills-and-valleys-in-an-array/2316-count-hills-and-valleys-in-an-array.java
+++ 2316-count-hills-and-valleys-in-an-array/2316-count-hills-and-valleys-in-an-array.java
@@ -1,33 +1,33 @@
 class Solution {
     public int countHillValley(int[] nums) {
-        int point = 1;
-        int hill = 0;
-        int valley = 0;
+        int count = 0;
 
-        List<Integer> list = new ArrayList<>();
-        if (nums.length > 0) {
-            list.add(nums[0]);
-        }
+        for (int i = 1; i < nums.length - 1; i++) {
+            // Skip if current is same as previous
+            if (nums[i] == nums[i - 1]) continue;
 
-        for (int i = 1; i < nums.length; i++) {
-            if (nums[i] != nums[i - 1]) {
-                list.add(nums[i]);
+            // Find previous distinct value
+            int prev = i - 1;
+            while (prev >= 0 && nums[prev] == nums[i]) {
+                prev--;
+            }
+
+            // Find next distinct value
+            int next = i + 1;
+            while (next < nums.length && nums[next] == nums[i]) {
+                next++;
+            }
+
+            // Ensure bounds are valid
+            if (prev >= 0 && next < nums.length) {
+                if (nums[i] > nums[prev] && nums[i] > nums[next]) {
+                    count++; // hill
+                } else if (nums[i] < nums[prev] && nums[i] < nums[next]) {
+                    count++; // valley
+                }
             }
         }
 
-        Integer[] arr = list.toArray(new Integer[0]);
-
-        while (point < arr.length-1) {
-            if (arr[point - 1] < arr[point] && arr[point + 1] < arr[point]) {
-                hill++;
-            }
-             if (arr[point - 1] > arr[point] && arr[point + 1] > arr[point]) {
-                valley++;
-            }
-
-            point++;
-        }
-
-        return hill+valley;
+        return count;
     }
 }
